b51c52346a13f38d1b25c5a312fe92a78eb471f6
Larry Osakwe <larryosak@gmail.com>
2024-04-18 22:55:03 -0700

    add billing api

--- packages/functions/src/delete.ts
+++ packages/functions/src/delete.ts
@@ -1,6 +1,7 @@
 import { Table } from "sst/node/table";
 import handler from "@notes/core/handler";
 import dynamoDb from "@notes/core/dynamodb";
+
 export const main = handler(async (event) => {
     const params = {
         TableName: Table.Notes.tableName,
